cd17849a29fbdcd379e92f13d82c478754489a01
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 15:02:09 -0500

    Integrar HistorialConversiones en la clase ConversorDeMoneda

--- src/ConversorDeMoneda.java
+++ src/ConversorDeMoneda.java
@@ -3,12 +3,13 @@ import com.google.gson.JsonParser;
 
 public class ConversorDeMoneda {
     private ConsultaTasaDeCambio consultaTasaDeCambio;
+    private HistorialConversiones historial;
 
     public ConversorDeMoneda() {
         consultaTasaDeCambio = new ConsultaTasaDeCambio();
+        historial = new HistorialConversiones();
     }
 
-    // Método para convertir una cantidad de una moneda a otra
     public double convertir(String monedaOrigen, String monedaDestino, double monto) {
         try {
             // Obtener la tasa de cambio en formato JSON
@@ -25,9 +26,15 @@ public class ConversorDeMoneda {
             // Calcular la cantidad en la moneda de destino
             double montoEnDestino = (monto / tasaOrigen) * tasaDestino;
 
+            // Agregar la conversión al historial
+            historial.agregarConversion(monedaOrigen, monedaDestino, monto, montoEnDestino);
             return montoEnDestino; // Retornar el monto convertido
         } catch (NullPointerException e) {
             throw new RuntimeException("Error en la conversión de monedas. Verifique las monedas ingresadas.", e);
         }
+    }
+
+    public HistorialConversiones getHistorial() {
+        return historial;
     }
 }
