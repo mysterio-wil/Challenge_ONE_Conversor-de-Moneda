59880c84f5db33c4f996ee51a81e69372c5f81d6
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 09:32:04 -0500

    Crear clase Agregar clave de API y URL de la API de tasas de cambio

--- src/ConsultaTasaDeCambio.java
+++ src/ConsultaTasaDeCambio.java
@@ -1,2 +1,4 @@
 public class ConsultaTasaDeCambio {
+    private final String apiKey = "44af044fffe9869c5dc9dd74"; // Reemplaza con tu API key
+    private final String urlApi = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
 }
