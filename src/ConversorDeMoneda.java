40f4c069088cd9574b5b33db13b75a8016d46b05
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 09:56:58 -0500

    Implementar método convertir en ConversorDeMoneda para convertir montos

--- src/ConversorDeMoneda.java
+++ src/ConversorDeMoneda.java
@@ -7,4 +7,23 @@ public class ConversorDeMoneda {
     public ConversorDeMoneda() {
         consultaTasaDeCambio = new ConsultaTasaDeCambio();
     }
+
+    // Método para convertir una cantidad de una moneda a otra
+    public double convertir(String monedaOrigen, String monedaDestino, double monto) {
+        // Obtener la tasa de cambio en formato JSON
+        String respuestaJson = consultaTasaDeCambio.obtenerTasaDeCambio(consultaTasaDeCambio.getUrlApi());
+
+        // Parsear la respuesta JSON para obtener la tasa de cambio
+        JsonObject jsonObject = JsonParser.parseString(respuestaJson).getAsJsonObject();
+        JsonObject tasasDeCambio = jsonObject.getAsJsonObject("conversion_rates");
+
+        // Obtener la tasa de cambio de las monedas de interés
+        double tasaOrigen = tasasDeCambio.get(monedaOrigen).getAsDouble();
+        double tasaDestino = tasasDeCambio.get(monedaDestino).getAsDouble();
+
+        // Calcular la cantidad en la moneda de destino
+        double montoEnDestino = (monto / tasaOrigen) * tasaDestino;
+
+        return montoEnDestino; // Retornar el monto convertido
+    }
 }
