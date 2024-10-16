d4c50987e45a7764b1e87c920a1509ed2a864edb
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 09:34:53 -0500

    Implementar método obtenerTasaDeCambio para realizar solicitudes a la API

--- src/ConsultaTasaDeCambio.java
+++ src/ConsultaTasaDeCambio.java
@@ -1,4 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ConsultaTasaDeCambio {
     private final String apiKey = "44af044fffe9869c5dc9dd74"; // Reemplaza con tu API key
     private final String urlApi = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+
+    // Método para realizar la consulta a la API de tasas de cambio
+    public String obtenerTasaDeCambio(String urlApi) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(this.urlApi))
+                .build();
+
+
+    }
 }
