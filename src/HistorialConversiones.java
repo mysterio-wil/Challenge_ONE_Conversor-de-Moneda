edf0352b128ab47d3ed94f6e5415944d1eef4bbb
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 14:49:03 -0500

    Se crea la clase HistorialConversiones, que contiene un atributo privado historial, una lista que almacenará las conversiones realizadas. Se inicializa la lista en el constructor de la clase.

---
+++ src/HistorialConversiones.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistorialConversiones {
+    private List<String> historial;
+
+    public HistorialConversiones() {
+        historial = new ArrayList<>();
+    }
+}
