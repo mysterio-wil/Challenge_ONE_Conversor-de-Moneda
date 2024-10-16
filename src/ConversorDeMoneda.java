4d2241ecabd87ea67fe5ff3165f1bdab6666aa28
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-16 09:52:29 -0500

    Agregar atributo consultaTasaDeCambio en ConversorDeMoneda

--- src/ConversorDeMoneda.java
+++ src/ConversorDeMoneda.java
@@ -2,5 +2,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class ConversorDeMoneda {
+    private ConsultaTasaDeCambio consultaTasaDeCambio;
 
+    public ConversorDeMoneda() {
+        consultaTasaDeCambio = new ConsultaTasaDeCambio();
+    }
 }
